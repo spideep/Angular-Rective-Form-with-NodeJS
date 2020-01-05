a699bfd7d578d5908983f709ee840a8846b6328b
Alexis Rengifo <alexisrengifo@gmail.com>
2020-01-05 18:19:19 -0500

    Display User Data component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputUserDataFormComponent
+    InputUserDataFormComponent,
+    DisplayUserDataComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-user-data/display-user-data.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { UserInfoModel } from '../models/UserInfoModel';
+
+@Component({
+  selector: 'display-user-data',
+  templateUrl: './display-user-data.component.html',
+  styleUrls: ['./display-user-data.component.scss']
+})
+export class DisplayUserDataComponent implements OnInit {
+
+  user: UserInfoModel = new UserInfoModel({
+    guid: "D21ds12x",
+    customerUid: "cust2dsa12dsa",
+    first_name: "Jhon",
+    last_name: "Doe",
+    email: "email@email.com",
+    zipcode: 10283,
+    password: "Idasn2x2#"
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
